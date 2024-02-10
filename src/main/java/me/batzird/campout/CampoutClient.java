71b5517dceccb3695ce20f0971b31aa19f018e68
BuildTools <unconfigured@null.spigotmc.org>
2024-02-10 16:47:21 -0600

    Initial commit

---
+++ src/main/java/me/batzird/campout/CampoutClient.java
@@ -0,0 +1,11 @@
+package me.batzird.campout;
+
+import  net.fabricmc.api.ClientModInitializer;
+
+public class CampoutClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
